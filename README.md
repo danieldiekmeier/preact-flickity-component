0fce15fed194d3407b85ee68d54f1fcdc32a403d
Daniel Diekmeier <fax@danieldiekmeier.de>
2018-03-20 12:16:20 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Preact Flickity Component
+
+This is an adaption of [@theolampert's](https://github.com/theolampert)[React Flickity Component] (https://github.com/theolampert/react-flickity-component) that is compatible to [Preact](preactjs.com).
+
+It doesn't use PropTypes, Portals or any of that fancy React stuff.
+
+I built it because I needed to mount a Flickity Slider, and I'm afraid that I will have to do it again at some point in my career.
+
+#### License Information:
+
+[GPLv3](https://www.gnu.org/licenses/gpl-3.0.html)
+
+Flickity may be used in commercial projects and applications with the one-time purchase of a commercial license.
+http://flickity.metafizzy.co/license.html
---
+++ src/FlickityComponent.js
@@ -0,0 +1,61 @@
+import { h, Component } from 'preact'
+import Flickity from 'flickity'
+
+export default class FlickityComponent extends Component {
+  constructor (props) {
+    super(props)
+
+    this.carousel = null
+    this.flkty = null
+  }
+
+  create () {
+    if (this.carousel) {
+      this.flkty = new Flickity(this.carousel, this.props.options)
+
+      if (this.props.flickityRef) {
+        this.props.flickityRef(this.flkty)
+      }
+    }
+  }
+
+  destroy () {
+    if (this.flkty) {
+      this.flkty.destroy()
+      this.flkty = null
+      this.carousel = null
+    }
+  }
+
+  componentWillUpdate () {
+    this.destroy()
+  }
+
+  componentDidUpdate () {
+    this.create()
+  }
+
+  componentWillUnmount () {
+    this.destroy()
+  }
+
+  componentDidMount () {
+    this.create()
+  }
+
+  render (props) {
+    return h(this.props.elementType, {
+      className: this.props.className,
+      ref: c => { this.carousel = c }
+    }, this.props.children)
+  }
+}
+
+FlickityComponent.defaultProps = {
+  options: {
+    pageDots: false,
+    wrapAround: true
+  },
+  className: '',
+  elementType: 'div'
+}
